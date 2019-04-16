7a741f333f70e6571c388929d8b6157998656c0b
Alexander <Philalex196401@gmail.com>
2019-04-16 18:48:58 +0300

    1.0.1
    1. hideKeaboard function - with tap after filling in searchBar - had been added. 2. ПоискButton and ПокупкиButton icons has been changed. 3. I'm still procrastinating with tableCell, very reluctant to use xib. May be I'll try to deal without xib. 4. Model with struct Item had been added.

---
+++ MealDealTest/MealDealTest/Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  MealDealTest
+//
+//  Created by Александр Филиппов on 16.04.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import UIKit
+
+struct Item {
+    let description: String
+    let price: Decimal
+    let discount: Int
+    var image: String
+    var retailer: String
+}
