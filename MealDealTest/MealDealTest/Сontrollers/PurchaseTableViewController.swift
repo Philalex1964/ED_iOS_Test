6e536eac59dbf77c9eb61109679e56e525413bd7
Alexander <Philalex196401@gmail.com>
2019-04-22 19:36:54 +0300

    1.0.1.5.2
    created MOC. Trying to fill data

--- MealDealTest/MealDealTest/Сontrollers/PurchaseTableViewController.swift
+++ MealDealTest/MealDealTest/Сontrollers/PurchaseTableViewController.swift
@@ -7,31 +7,22 @@
 //
 
 import UIKit
+import CoreData
 
 class PurchaseTableViewController: UITableViewController {
         
     @IBOutlet weak var purchaseTableView: UITableView!
     
-    public var items: [Item] = [
-        Item(description: "Молоко Простоквашино", price: 49.99, discount: 15, itemImageName: "40", retailer: "Лента"),
-    ]
+    var item: ItemMO!
+    
+    public var items: [ItemMO] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
@@ -43,7 +34,7 @@ class PurchaseTableViewController: UITableViewController {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: PurchaseCell.reuseId, for: indexPath) as? PurchaseCell else { fatalError("Cell cannot be dequeued")}
 
         cell.purchaseDescriptionLabel.text = items[indexPath.row].description
-        cell.purchaseItemImage.image = UIImage(named: items[indexPath.row].itemImageName!)
+        cell.purchaseItemImage.image = UIImage(named: items[indexPath.row].imageName!)
         cell.purchaseRetailerLabel.text = items[indexPath.row].retailer
         cell.purchasePriceLabel.text = "\(items[indexPath.row].price)"
         cell.purchaseDiscountLabel.text = String(format:"%d", items[indexPath.row].discount)
@@ -52,48 +43,12 @@ class PurchaseTableViewController: UITableViewController {
     }
   
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+    
  
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for addToCartSeague: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+ 
     }
     
     
--- MealDealTest/MealDealTest/Сontrollers/SearchTableViewController.swift
+++ MealDealTest/MealDealTest/Сontrollers/SearchTableViewController.swift
@@ -7,18 +7,19 @@
 //
 
 import UIKit
+import CoreData
 
 class SearchTableViewController: UITableViewController {
     
     @IBOutlet weak var searchTableView: UITableView!
     
+    var item: ItemMO!
     
+    public var items: [ItemMO] = []
     
-    public var items: [Item] = [
-        Item(description: "Соки Сады Придонья", price: 69.99, discount: 25, itemImageName: "10", retailer: "Metro"),
-        Item(description: "Кока-Кола", price: 49.99, discount: 15, itemImageName: "20", retailer: "Ашан"),
-        Item(description: "Grant's", price: 1999.99, discount: 10, itemImageName: "30", retailer: "G5")
-    ]
+    let appDelegate = (UIApplication.shared.delegate as? AppDelegate)
+   
+
     
 
     override func viewDidLoad() {
@@ -27,11 +28,6 @@ class SearchTableViewController: UITableViewController {
         let tapGR = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         searchTableView.addGestureRecognizer(tapGR)
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -48,10 +44,6 @@ class SearchTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
@@ -61,7 +53,7 @@ class SearchTableViewController: UITableViewController {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: ItemCell.reuseId, for: indexPath) as? ItemCell else { fatalError("Cell cannot be dequeued")}
 
         cell.descriptionLabel.text = items[indexPath.row].description
-        cell.itemImage.image = UIImage(named: items[indexPath.row].itemImageName!)
+        cell.itemImage.image = UIImage(named: items[indexPath.row].imageName!)
         cell.retailerLabel.text = items[indexPath.row].retailer
         cell.priceLabel.text = "\(items[indexPath.row].price)"
         cell.discountLabel.text = String(format:"%d", items[indexPath.row].discount)
@@ -70,42 +62,7 @@ class SearchTableViewController: UITableViewController {
     }
  
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+   
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -113,7 +70,7 @@ class SearchTableViewController: UITableViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-     */
+ 
     
     
    
